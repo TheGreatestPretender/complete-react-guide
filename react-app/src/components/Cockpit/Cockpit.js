f295aaf7d8537e1f446780985ce092daed9c188d
TheGreatestPretender <katschmidt201@gmail.com>
2020-07-14 19:56:59 -0400

    finished section 7

--- react-app/src/components/Cockpit/Cockpit.js
+++ react-app/src/components/Cockpit/Cockpit.js
@@ -1,13 +1,30 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import classes from './Cockpit.css';
 
+import AuthContext from '../../context/authContext';
+
 const cockpit = (props) => {
+
+	const toggleBtnRef = useRef(null);
+
 	useEffect(() => {
 		console.log('[Cockpit.js] useEffect');
+
+		toggleBtnRef.current.click();
+		return () => {
+			console.log('[Cockpit.js] cleanup work in useEffect');
+		};
+	}, []);
+
+
+	useEffect(() => {
+		console.log('[Cockpit.js] useEffect 2');
+		return () => {
+			console.log('[Cockpit.js] cleanup work in useEffect 2');
+		};
 	});
-
-
+ 
 	const assignedClasses = [];
 	if (props.persons.length <= 2) {
 		assignedClasses.push(classes.red); // classes = ['red']
@@ -26,9 +43,15 @@ const cockpit = (props) => {
 		<div className={classes.Cockpit}>
 			<h1>{props.title}</h1>
 			<p className={assignedClasses.join(' ')}>This is really working!</p>
-			<button className={btnClass} onClick={props.clicked}>
+			<button ref={toggleBtnRef} className={btnClass} onClick={props.clicked}>
 			Toggle Persons
 			</button>
+
+			<AuthContext.Consumer>
+				{(context) => <button onClick={context.login}>Log In</button>}
+			</AuthContext.Consumer>
+
+			
 		</div>
 	)
 }
--- react-app/src/container/App.js
+++ react-app/src/container/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons'; 
 import Cockpit from '../components/Cockpit/Cockpit';
+import AuthContext from '../context/authContext';
 //import ErrorBoundary from '../components/ErrorBoundary/ErrorBoundary';
 //import WithClass from '../hoc/WithClass';
 
@@ -22,7 +23,8 @@ class App extends Component {
     otherState: 'some other value',
     showPersons: false,
     showCockpit: true,
-    changeCounter: 0
+    changeCounter: 0,
+    authenticated: false
   };
 
   static getDerivedStateFromProps(props, state) {
@@ -70,6 +72,10 @@ class App extends Component {
     this.setState({ showPersons: !doesShow });
   };
 
+  loginHandler = () => {
+    this.setState({authenticated:true});
+  }
+
   render() {
     console.log('[App.js] render');
     let persons = null;
@@ -79,7 +85,8 @@ class App extends Component {
           <Persons 
             persons={this.state.persons} 
             clicked={this.deletePersonHandler}
-            changed={this.nameChangedHandler} />
+            changed={this.nameChangedHandler}
+            isAuthenticated={this.state.authenticated} />
       );
     }
     return (
@@ -87,15 +94,18 @@ class App extends Component {
         <button onClick={() => {this.setState({showCockpit: false});}}>
           Remove Cockpit
         </button>
-
-        {this.state.showCockpit ? (
-          <Cockpit 
-            title={this.props.appTitle}
-            showPersons={this.state.showPersons} 
-            persons={this.state.persons} 
-            clicked={this.togglePersonsHandler}/>)
-            : null}
-        {persons}
+        <AuthContext.Provider value={{
+          authenticated: this.state.authenticated,
+          login: this.loginHandler}}>
+          {this.state.showCockpit ? (
+            <Cockpit 
+              title={this.props.appTitle}
+              showPersons={this.state.showPersons} 
+              persons={this.state.persons} 
+              clicked={this.togglePersonsHandler}/>)
+              : null}
+          {persons}
+        </AuthContext.Provider>
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
