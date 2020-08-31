650e6a88afd9ec919b23a9a96e70e01940bee817
Kat Schmidt <katschmidt201@gmail.com>
2020-08-31 19:44:12 -0400

    Redux added to counter container
    Added mapStateToProps and mapDispatchToProps with different reference to a functions that dispatches a function as an action

--- example_projects/redux--01-start/src/containers/Counter/Counter.js
+++ example_projects/redux--01-start/src/containers/Counter/Counter.js
@@ -28,14 +28,43 @@ class Counter extends Component {
     render () {
         return (
             <div>
-                <CounterOutput value={this.state.counter} />
-                <CounterControl label="Increment" clicked={() => this.counterChangedHandler( 'inc' )} />
-                <CounterControl label="Decrement" clicked={() => this.counterChangedHandler( 'dec' )}  />
-                <CounterControl label="Add 5" clicked={() => this.counterChangedHandler( 'add', 5 )}  />
-                <CounterControl label="Subtract 5" clicked={() => this.counterChangedHandler( 'sub', 5 )}  />
+                <CounterOutput value={this.props.ctr} />
+                <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
+                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
+                <CounterControl label="Add 5" clicked={this.props.onAddCounter}  />
+                <CounterControl label="Subtract 5" clicked={this.props.onSubtractCounter}  />
+                <hr />
+                <button onClick={this.props.onStoreResult}>Store Result</button>
+                <ul>
+                    {this.props.storedResults.map((result) => 
+                        <li key={result.id} onClick={() => this.props.onDeleteResult(result.id)}>
+                            {result.value}
+                        </li>
+                    )}
+                    
+                </ul>
             </div>
         );
     }
 }
 
-export default Counter;
+const mapStateToProps = state => {
+    return {
+        ctr: state.counter,
+        storedResults: state.results
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    //holds a reference to a function that dispatches an action
+    return {
+        onIncrementCounter: () => dispatch({type: 'INCREMENT'}),
+        onDecrementCounter: () => dispatch({type: 'DECREMENT'}),
+        onAddCounter: () => dispatch({type: 'ADD', value: 5}),
+        onSubtractCounter: () => dispatch({type: 'SUB', value: 5}),
+        onStoreResult: () => dispatch({type: 'STORE_RESULT'}),
+        onDeleteResult: (id) => dispatch({type: 'DELETE_RESULT', resultId: id})
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
