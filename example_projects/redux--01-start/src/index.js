72d2d5987fc6b90268838a83e26e5f8d7fac576a
TheGreatestPretender <katschmidt201@gmail.com>
2020-09-03 10:47:55 -0400

    WIP

--- example_projects/redux--01-start/src/index.js
+++ example_projects/redux--01-start/src/index.js
@@ -2,7 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
+import counterReducer from './store/reducers/counter';
+import resultReducer from './store/reducers/result';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootReducer = combineReducers({
+    ctr: counterReducer,
+    res: resultReducer
+})
+
+const store = createStore(rootReducer);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
